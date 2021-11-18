69314f923eef70d4431c125aa8e309ad085910e4
유국현 <rnrgus5897@gmail.com>
2021-11-18 14:49:14 +0900

    cafeProject

---
+++ pages/introduce.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+
+export default function introduce() {
+    return (
+        <div className='container'>
+             <Header />
+
+  
+
+        <h1>Introduce</h1>
+        </div>
+    )
+}
