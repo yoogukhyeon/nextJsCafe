6d30e233c3a26f7aa68653cce6ca5d660a71a7b5
유국현 <rnrgus5897@gmail.com>
2021-11-19 19:04:10 +0900

    api 처리

---
+++ pages/api/menu.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+    const file = fs.readFileSync(path.resolve( './assets/menu.csv'),  'utf-8')
+    console.log(file)
+    const rows = file.split('\n')
+    console.log(rows)
+
+    const json = [];
+    rows.forEach(row => {
+        if(row === '') return
+        const item = row.split(',')
+        console.log(item)
+        json.push({
+            name : item[0],
+            price : parseInt(item[1] , 10)
+        })
+    })
+
+ 
+    res.status(200).json(json)
+  }
+  
